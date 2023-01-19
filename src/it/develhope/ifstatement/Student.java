eb199cb549f3d023652a87cc35bfe2e5da9b891e
Giulia <giuliac35@gmail.com>
2023-01-19 12:51:29 +0100

    Added if statement to guessingAge method

--- src/it/develhope/ifstatement/Student.java
+++ src/it/develhope/ifstatement/Student.java
@@ -9,8 +9,17 @@ public class Student {
         this.age = studentAge;
     }
 
-    public void guessingAge(){
-        Random random = new Random(age);
+    public void guessingAge(int rando){
+        Random random = new Random(rando);
         int random_integer = random.nextInt(35);
+        System.out.printf("I generated the random number %d for student %s", rando, name);
+
+        if(rando == age){
+            System.out.println("The random number is equal to the student's age");
+        }else if(rando > age){
+            System.out.println("The random number is greater than the student's age");
+        }else{
+            System.out.println("The random number is lower than the student's age");
+        }
     }
 }
