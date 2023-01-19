acdfa6d437b4083cd8f4f9e1a469eee70e7a7956
Giulia <giuliac35@gmail.com>
2023-01-19 12:41:37 +0100

    Define guessingAge method, import java.util.Random

--- src/it/develhope/ifstatement/Student.java
+++ src/it/develhope/ifstatement/Student.java
@@ -1,11 +1,16 @@
 package it.develhope.ifstatement;
-
+import java.util.Random;
 public class Student {
     private String name;
     private int age;
 
-    public Student(String name, int age){
-        this.name = name;
-        this.age = age;
+    public Student(String studentName, int studentAge){
+        this.name = studentName;
+        this.age = studentAge;
+    }
+
+    public void guessingAge(){
+        Random random = new Random(age);
+        int random_integer = random.nextInt(35);
     }
 }
